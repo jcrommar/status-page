491590dd410aa82056df3ce34d07be899b9064c9
JC Romualdo <juan.romualdo@zoro.com>
2019-05-03 10:24:54 -0500

    src files and components for application

---
+++ src/components/general/NotificationBar.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+    <div
+    v-for="i in notificationErrors"
+    :key="i.id"
+    :class="'full-background ' + i.color"
+    >
+        {{i.text}}
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'NotificationBar',
+    data() {
+        return {
+            notificationErrors: [{'color': 'red', id: 0, text: 'Sorry, we seem to be having issues'}]
+        }
+    }
+}
+</script>
+
+<style scoped>
+.full-background {
+    padding: 10px;
+    z-index: 0;
+    position: relative;
+    text-align: center;
+    color: white;
+}
+
+</style>
---
+++ src/components/home/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="cards">
+      <div
+        class="card incidents"
+        v-if="openIncidents.lenght > 0"
+        >
+        <div
+        class="incident_item"
+        v-for="i in openIncidents"
+        :key="i.id"
+        >
+          <router-link :to="{ name: 'IncidentDetail', params: { id: i.id}}">
+            <IncidentListItem :incident="i" />
+          </router-link>
+        </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import IncidentListItem from './IncidentListItem.vue'
+export default {
+  name: 'Home',
+  data () {
+    return {
+          openIncidents: [{'id': 1, 'title': 'Connectivity Issues', update_set: [{'description': 'We have noticed some connectivity issues', 'date': '2019-05-03 09:00:00', 'status': 'Investigating'}]}]
+    }
+  },
+  components: {IncidentListItem}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
